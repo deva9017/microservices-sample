b6a24bacc51fdba92376d058382d01de40261bb3
devendra-suntechno <k.devendra@suntechnoservices.com>
2021-03-17 16:17:59 +0530

    ip added

--- web-application/e2e/protractor.conf.js
+++ web-application/e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://3.6.49.194:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
